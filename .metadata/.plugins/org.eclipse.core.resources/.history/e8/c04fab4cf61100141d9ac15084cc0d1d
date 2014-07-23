25f8d915fc30fed20b5780e217d24774d6002bf3
Your Name <you@example.com>
2014-07-23 08:36:18 +0000

    modified:   .metadata/.log 	new file:   .metadata/.plugins/org.eclipse.core.resources/.history/0/00b0fb0f1a1200141d9ac15084cc0d1d

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/e8/c04fab4cf61100141d9ac15084cc0d1d
@@ -0,0 +1,20 @@
+
+
+
+public class Airplane {
+
+	public static void main(String[] args) {
+	
+		
+	
+	  
+   
+  gbD.getD(55.786267, 37.543344,35.443232, 139.633913);
+ // System.out.println( GetDistance.getAzimut(53.393055555556, 58.755555555556,55.3,61.5));
+//  GetDistance.DesPoint(53.393055555556, 58.755555555556,276, 38);
+ // brakeDistance.brake(53.393055555556, 58.755555555556,2760, 46);
+	//  System.out.println(p.DesPoint())	;
+	  
+	  
+	  }
+}
--- Airplane/src/GetDistance.java
+++ Airplane/src/GetDistance.java
@@ -6,7 +6,11 @@ public class GetDistance {
 	
   //вычесление длины большого круга 
    
-	static  public double getDist(double lat1 , double lon1 , double lat2 , double lon2) {
+	
+	
+
+    
+  static  public double getDist(double lat1 , double lon1 , double lat2 , double lon2) {
     
     	double rlat2=Math.toRadians(lat2);	
         double rlat1=Math.toRadians(lat1);
@@ -25,44 +29,48 @@ public class GetDistance {
 
 
 
-// вычесление  начального азимута
+
 
  static   public double getAzimut (double lat1, double lon1 ,double lat2 , double lon2 ) {
 		 
     	double rlat2=Math.toRadians(lat2);	
-        double rlat1=Math.toRadians(lat1);
-        double rlon1=Math.toRadians(lon1);
+       double rlat1=Math.toRadians(lat1);
+       double rlon1=Math.toRadians(lon1);
         double rlon2=Math.toRadians(lon2);
        
 	
  
-        return  (int)  ( Math.toDegrees(Math.atan2(Math.sin(rlon2-rlon1)*Math.cos(rlat2),
+   return   (int)  Math.toDegrees(Math.atan2(Math.sin(rlon2-rlon1)*Math.cos(rlat2),
         			                Math.cos(rlat1)*Math.sin(rlat2) - Math.sin(rlat1)*Math.cos(rlat2)*Math.cos(rlon2-rlon1)
-        			           ) ) );
+        			            ) );
        		        
 
     }
  
- // расчет конечной точки при  заданых начальном азимуте и растояние
  
- static   public double DesPoint (double lat1 , double lon1 , int d , double b)
+	   
+	
+
+	 
+    static   public double DesPoint (double lat1 , double lon1 , int d , double b)
  
  {
 	 	
      double rlat1=Math.toRadians(lat1);
      double rlon1=Math.toRadians(lon1);
      
-     double dist = d/6372.0;
+     double dist = 150.0/6372.0;
      double brng = Math.toRadians(b);
     
 
      double lat2 = Math.asin( Math.sin(rlat1)*Math.cos(dist) + Math.cos(rlat1)*Math.sin(dist)*Math.cos(brng) );
      double a = Math.atan2(Math.sin(brng)*Math.sin(dist)*Math.cos(rlat1), Math.cos(dist)-Math.sin(rlat1)*Math.sin(lat2));
+     System.out.println("a = " +  a);
      double lon2 = rlon1 + a;
 
      lon2 = (lon2+ 3*Math.PI) % (2*Math.PI) - Math.PI;
 
-     System.out.println("Latitude  = "+Math.toDegrees(lat2)+"\nLongitude = "+Math.toDegrees(lon2));
+     System.out.println("Latitude = "+Math.toDegrees(lat2)+"\nLongitude = "+Math.toDegrees(lon2));
     
  return 1;
 
